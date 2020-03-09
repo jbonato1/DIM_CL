ba37a0374712d85645eaf9dccef5d8cce21fed7c
vargas95 <alessandro17mv@gmail.com>
2020-03-09 14:29:07 +0100

    cutmix

--- DIM/train.py
+++ DIM/train.py
@@ -14,6 +14,7 @@ from torch.nn.parameter import Parameter
 
 ###
 from DIM.dim_loss import *
+from DIM.train_prior_disc import train_disc,sample_prior
 
 
 def print_metrics(metrics, batch_num, phase):    
@@ -31,6 +32,15 @@ def update_metrics(loss,metrics):
     metrics['loss'] += loss.data.cpu().numpy()
     
 
+def compute_loss(pred,y,metrics):
+    """Compute accuracy and cross entropy loss for classifier"""
+    loss = F.cross_entropy(pred,y)
+    metrics['loss'] += loss.data.cpu().numpy()    
+    pred_l = pred.data.cpu().numpy()
+    metrics['accuracy'] +=np.sum(np.argmax(pred_l,axis=1)==y.data.cpu().numpy())/pred_l.shape[0] 
+    return loss
+    
+    
 def train(model, optimizer, scheduler,dataloaders,device,kwargs):
     """This funcion performs training of DIM local version without prior distrib. It can be wrapped into the DNN
        module as a class function  
@@ -40,8 +50,19 @@ def train(model, optimizer, scheduler,dataloaders,device,kwargs):
     writer=kwargs['writer']
     best_loss=kwargs['best_loss']
     t_board=kwargs['t_board']
-    scale_loss = kwargs['gamma']
+    gamma = kwargs['gamma']
+    beta = kwargs['beta']
+    use_prior = kwargs['Prior_Flag'] 
+    discriminator = kwargs['discriminator'] 
+    optimizerD = kwargs['optimizerD'] 
+    samples_path = kwargs['samples_path']
+#     use_cutmix = kwargs['Cutmix_Flag']
+#     cutmix_prob = kwargs['cutmix_prob']
+#     cutmix_beta = kwargs['cutmix_beta']
     
+    sampler = sample_prior(samples_path,device)
+    gen_epoch = 0
+    gen_epoch_l=0
     
     best_model_wts = copy.deepcopy(model.state_dict())
     
@@ -69,28 +90,48 @@ def train(model, optimizer, scheduler,dataloaders,device,kwargs):
                 optimizer.zero_grad()
                 # forward
                 # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):                    
+                with torch.set_grad_enabled(phase == 'train'):
+                                                
                     E_phi,C_phi = model(inputs)
                     #print('first', torch.cuda.max_memory_allocated(),torch.cuda.max_memory_cached())
                     loss = 0   
                     #we use jsd MI approx. since it is more stable eventually for other exp call compute dim loss
                     #function already implemented
-                    loss = -1*fenchel_dual_loss(C_phi, E_phi, measure='JSD') 
+                    loss = fenchel_dual_loss(C_phi, E_phi, measure='JSD') 
                     update_metrics(loss,metrics)
+                    ################################## section for prior Loss
+                    if use_prior:
+                        #definition of X_Q (current samples) as features extracted by the encoder E_phi 
+                        #in the paper there is also a sigmoid applied maybe it can be tested
+                        X_P = sampler(size)
+                        loss*=beta
+                        if phase=='train':
+                            discriminator,Q_samples= train_disc(discriminator,X_P,E_phi,optimizerD,metrics,gradient_penalty=1.0)
+                        else:
+                            Q_samples = discriminator(E_phi)
+                            
+                        prior_loss = generator_loss(Q_samples, measure='GAN', loss_type='non-saturating')
+                        loss += gamma*prior_loss
+                        
+                    ################################## backward and tensorboard: othre quantities can be added
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
-                        if batch_num % 100 == 99 and t_board:    
+                        if batch_num % 20 == 0  and t_board:    
+                            #print(epoch*gen_epoch+batch_num)
                             # ...log the running loss
-                            writer.add_scalar('training loss',loss,batch_num)
+                            writer.add_scalar('training loss',loss,epoch*gen_epoch+batch_num)
                     if phase == 'val' and t_board:
-                        if batch_num % 100 == 99:    
+                        if batch_num % 10 == 0:    
                             # ...log the running loss
-                            writer.add_scalar('Validation loss',loss,batch_num)
+                            writer.add_scalar('Validation loss',loss,epoch*gen_epoch_l+batch_num)
 
                 # statistics
                 batch_num +=1
-           
+            if epoch ==0 and phase=='train':
+                gen_epoch = batch_num
+            if epoch ==0 and phase=='val':
+                gen_epoch_l = batch_num
             if phase == 'train':
                 
                 avg = metrics['loss']/batch_num
@@ -121,6 +162,252 @@ def train(model, optimizer, scheduler,dataloaders,device,kwargs):
 
     del best_model_wts
     return model
+
+
+
+##### Eventually think about a generale trainer class
+
+def trainEnc_MI(model, optimizer, scheduler,dataloaders,device,kwargs):
+    """This funcion performs training of DIM local version without prior distrib. It can be wrapped into the DNN
+       module as a class function  
+    """
+    num_epochs=kwargs['ep']
+    writer=kwargs['writer']
+    best_loss=kwargs['best_loss']
+    t_board=kwargs['t_board']
+    gamma = kwargs['gamma']
+    beta = kwargs['beta']
+    use_prior = kwargs['Prior_Flag'] 
+    discriminator = kwargs['discriminator'] 
+    
+    gen_epoch = 0
+    gen_epoch_l=0
+    
+    best_model_wts = copy.deepcopy(model.state_dict())
+    
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+        
+        since = time.time()
+        
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            
+            if use_prior:
+                discriminator.requires_grad_(False)
+                
+            if phase == 'train':    
+                model.train()  # Set model to training mode                
+            else:
+                model.eval()   # Set model to evaluate mode
+
+
+            metrics = defaultdict(float)
+            epoch_samples = 0
+            batch_num = 0
+            for inputs, labels in dataloaders[phase]:
+                labels = labels.type(torch.long)
+                torch.cuda.empty_cache()
+                inputs = inputs.to(device)
+                labels = labels.to(device)        
+                optimizer.zero_grad()
+                
+                # forward
+                with torch.set_grad_enabled(phase == 'train'):                    
+                    E_phi,C_phi,A_phi = model(inputs)
+                    loss = 0   
+                    #we use jsd MI approx. since it is more stable eventually for other exp call compute dim loss
+                    #function already implemented
+                    loss_MI = fenchel_dual_loss(C_phi, E_phi, measure='JSD')
+                    
+                    if use_prior:
+                        loss += beta*loss_MI
+                    else:
+                        loss = loss_MI
+                    metrics['MI_loss'] += loss.data.cpu().numpy()                    
+                    ################################## section for prior Loss
+                    if use_prior:
+                        Q_samples = discriminator(A_phi)
+                        disc_loss = compute_loss(Q_samples,labels,metrics)
+                        loss += gamma*disc_loss
+                        
+                    metrics['loss'] += loss.data.cpu().numpy()
+                    ################################## backward and tensorboard: othre quantities can be added
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+                        if batch_num % 20 == 0  and t_board:    
+                            # ...log the running loss
+                            writer.add_scalar('training loss',loss,epoch*gen_epoch+batch_num)
+                    if phase == 'val' and t_board:
+                        if batch_num % 10 == 0:    
+                            # ...log the running loss
+                            writer.add_scalar('Validation loss',loss,epoch*gen_epoch_l+batch_num)
+
+                # statistics
+                batch_num +=1
+            ############ end of epochs
+            
+            epoch_loss = metrics['loss']/batch_num
+            
+            if epoch ==0 and phase=='train':
+                gen_epoch = batch_num
+            if epoch ==0 and phase=='val':
+                gen_epoch_l = batch_num
+                
+            if phase == 'train':
+                avg = metrics['loss']/batch_num
+                scheduler.step()#avg
+
+                for param_group in optimizer.param_groups:
+                    print("LR", param_group['lr'])
+                    
+            #### Write and Save
+            print_metrics(metrics, batch_num, phase)
+            if phase == 'val' and epoch_loss < best_loss:
+                print("saving best model")
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+        
+        ############## Time Info
+        time_elapsed = time.time() - since
+        print('{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+        
+    print('Best val loss: {:4f}'.format(best_loss))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+
+    del best_model_wts
+    return model
+
+def rand_bbox(size, lam):
+    L = size[1]
+
+    cut_rat = np.sqrt(1. - lam)
+    cut_l = np.int(L * cut_rat)
+
+    # uniform
+    cx = np.random.randint(L)
+
+    bbx1 = np.clip(cx - cut_l // 2, 0, L)
+    bbx2 = np.clip(cx + cut_l // 2, 0, L)
+    return bbx1, bbx2
+
+def train_classifier(model, optimizer, scheduler,dataloaders,device,kwargs):
+    """This funcion performs training of classifier HEAD. It can be wrapped into the DNN
+       module as a class function  
+    """
+        
+    num_epochs=kwargs['ep']
+    writer=kwargs['writer']
+    best_loss=kwargs['best_loss']
+    t_board=kwargs['t_board']
+    use_cutmix=kwargs['Flag_cutmix']
+    cutmix_beta=kwargs['cutmix_beta']
+    cutmix_prob=kwargs['cutmix_prob']
+    
+    gen_epoch = 0
+    gen_epoch_l=0
+    
+    best_model_wts = copy.deepcopy(model.state_dict())
+    
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+        
+        since = time.time()
+        
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':    
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            metrics = defaultdict(float)
+            epoch_samples = 0
+            batch_num = 0
+            for inputs, labels in dataloaders[phase]:
+                labels = labels.type(torch.long)
+                torch.cuda.empty_cache()
+                inputs = inputs.to(device)
+                labels = labels.to(device)        
+                optimizer.zero_grad()
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):                    
+                    
+                    ################################## CutMix loss
+                    if use_cutmix:
+
+                        r = np.random.rand(1)
+                        if cutmix_beta > 0 and r < cutmix_prob:
+                            # generate mixed sample
+                            lam = np.random.beta(cutmix_beta, cutmix_beta)
+                            rand_index = torch.randperm(inputs.size()[0]).cuda()
+
+                            labels_a = labels
+                            labels_b = labels[rand_index]
+
+                            bbx1, bbx2 = rand_bbox(inputs.size(), lam)
+
+                            inputs[:, bbx1:bbx2] = inputs[rand_index, bbx1:bbx2]
+                            # adjust lambda to exactly match pixel ratio
+                            lam = 1 - ((bbx2 - bbx1) / inputs.size()[-1] )
+
+                            # compute output
+                            out = model(inputs)
+                            loss = 0
+                            loss = compute_loss(out, labels_a,metrics) * lam + compute_loss(out, labels_b,metrics) * (1. - lam)
+                    else:  
+                        out = model(inputs)
+                        loss = 0   
+                        loss = compute_loss(out,labels,metrics)
+
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+                            if batch_num % 20 == 0  and t_board:    
+                                writer.add_scalar('training loss',loss,epoch*gen_epoch+batch_num)
+
+                        if phase == 'val' and t_board:
+                            if batch_num % 10 == 0:    
+                                writer.add_scalar('Validation loss',loss,epoch*gen_epoch_l+batch_num)
+
+                # statistics
+                batch_num +=1
+            epoch_loss = metrics['loss']/batch_num
+            
+            if epoch ==0 and phase=='train':
+                gen_epoch = batch_num
+            if epoch ==0 and phase=='val':
+                gen_epoch_l = batch_num
+            if phase == 'train':
+                scheduler.step()#avg
+                for param_group in optimizer.param_groups:
+                    print("LR", param_group['lr'])
+            
+            
+            print_metrics(metrics, batch_num, phase)
+            
+            if phase == 'val' and epoch_loss < best_loss:
+                print("saving best model")
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        time_elapsed = time.time() - since
+
+        print('{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val loss: {:4f}'.format(best_loss))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+
+    del best_model_wts
+    return model
+
 
 
 def compute_dim_loss(l_enc, m_enc, measure, mode):
